2a25226ee184351d5c24497dc18822673047e3a9
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2023-03-13 17:32:45 +0000

    Fixed script for creating all test jobs

--- build_workflow/create_workflow.py
+++ build_workflow/create_workflow.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
    for line in inp.splitlines() :
        if "replica:" in line :
           alltests = os.listdir("../tests")
-          of.write( line + " [" + alltests[0] )
-          for i in range(1,len(alltests)) : of.write(", " + alltests[i])
+          of.write( line + " [\"" + alltests[0] +"\"" )
+          for i in range(1,len(alltests)) : of.write(", \"" + alltests[i] + "\"")
           of.write("]\n")
        else : of.write( line + "\n")
    of.close()
