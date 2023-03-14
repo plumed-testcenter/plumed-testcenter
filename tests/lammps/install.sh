c9a25e5e767b10b93910782397bc958592992e7c
Gareth Aneurin Tribello <garethtribello@gareths-mbp.mp.qub.ac.uk>
2023-03-14 11:39:35 +0000

    First attempt at adding stuff to build lammps

---
+++ tests/lammps/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+set -x
+
+plumed_mode="static"
+suffix=""
+
+for opt
+do
+case "$opt" in
+  (mode=*) mode="${opt#mode=}" ;;
+  (suffix=*) suffix="${opt#suffix=}" ;;
+  (*) echo "unknown option $opt" ; exit 1 ;;
+esac
+done
+
+# Cloning the lammps repository
+repo=https://github.com/lammps/lammps.git
+echo "cloning repoisitory $repo"
+git clone $repo
+
+# Finding the latest stable version of lammps to build
+cd lammps
+version=$(git tag --sort=-creatordate | grep stable | head -n 1)
+echo "installing latest stable lammps $version"
+git checkout $version
+
+# Building lammps using cmake
+mkdir build
+cd build
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/opt/lib/pkgconfig
+cmake -D PKG_MANYBODY=yes -D PKG_KSPACE=yes -D PKG_MOLECULE=yes -D PKG_RIGID=yes -D PKG_USER-PLUMED=yes -D PLUMED_MODE $plumed_mode ../cmake
+if [ ! -f ./lmp ] ; then
+     exit 1
+fi
+cp ./lmp $HOME/opt/bin/lammps$suffix
