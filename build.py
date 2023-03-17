735f63869b5ed46279a12f53126af4a11ab2e47b
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2023-03-17 20:54:41 +0000

    Modifying the build script to show in the install scripts

--- build.py
+++ build.py
@@ -22,14 +22,24 @@ def buildBrowsePage( tested ) :
    f.write("| Name of Program  | Compiles | Passes tests |\n")
    f.write("|:----------------:|:--------:|:------------:|\n")
    for code in os.listdir("tests") :
+       of = open( "tests/" + code + "/install.md", "w+" )
+       of.write("Compiling " + code + "\n")
+       of.write("------------------------\n \n")
+       of.write("To compile " + code + " and PLUMED the following bash script was used\n \n")
+       of.write("```bash\n")
+       sf = open("tests/" + code + "/install.sh" ,"r")
+       inp = sf.read()
+       for line in inp.splitlines() : of.write( line + "\n")
+       of.write("```\n")
+       of.close()
        compile_badge, test_badge = "", ""
        for i in range(len(tested)):
            compile_badge = compile_badge + ' [![tested on ' + tested[i] + '](https://img.shields.io/badge/' + tested[i] + '-'
            compile_badge = compile_badge + 'passing-green.svg'
-           compile_badge = compile_badge + '(www.youtube.com)'
+           compile_badge = compile_badge + ')](tests/' + code + '/install.md)'
            test_badge = test_badge + ' [![tested on ' + tested[i] + '](https://img.shields.io/badge/' + tested[i] + '-'
            test_badge = test_badge + 'passing-green.svg'
-           test_badge = test_badge + '(www.youtube.com)'
+           test_badge = test_badge + ')](www.youtube.com)'
        f.write("| [" + code + "](www.youtube.com) | " + compile_badge + " | " + test_badge + " | \n")  
    f.write(" \n")
    f.write("**Building PLUMED**\n")
