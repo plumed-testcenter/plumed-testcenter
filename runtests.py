55d731e97b6e1818692e881661856a8a1c581ac1
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2023-04-07 12:35:53 +0100

    Fixed another bug

--- runtests.py
+++ runtests.py
@@ -67,7 +67,7 @@ def runMDCalc( name, code, version, runner, params ) :
     # Check if the name is the stable version 
     if stable_version in params["executible"] : params["executible"] = runner.getExecutibleName()
     # Now test that the executable exists if it doesn't then the test is broken
-    if shutil.which(executible) == None : return True
+    if shutil.which(params["executible"]) == None : return True
     # Copy all the input needed for the MD calculation
     shutil.copytree("tests/" + code + "/input", "tests/" + code + "/" + name + "_" + version ) 
     # Change to the directory to run the calculation
