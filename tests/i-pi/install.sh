c4bf97196a7431ae5aa1754353d8e2f7d994cfa3
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-05-29 21:04:04 +0100

    Added lines in i-pi script to copy i-pi code

--- tests/i-pi/install.sh
+++ tests/i-pi/install.sh
@@ -6,8 +6,12 @@ cd i-pi/drivers/f90
 make
 cd ../../../
 
+# Copy i-pi and i-pi driver to $HOME/opt/bin
+cp bin/i-pi $HOME/opt/bin/i-pi-code
+cp bin/i-pi-driver $HOME/opt/bin/
+
 # Make a script to run i-pi
 echo "#!/bin/bash" > $HOME/opt/bin/i-pi
 echo "PYTHONPATH=$HOME/opt/lib/plumed$suffix/python" >> $HOME/opt/bin/i-pi
-echo "$HOME/i-pi/bin/i-pi install.xml & sleep 5; $HOME/i-pi/bin/i-pi-driver -m sg -h localhost -o 15 -p 31415" >> $HOME/opt/bin/i-pi
+echo "$HOME/opt/bin/i-pi-code install.xml & sleep 5; $HOME/opt/bin/i-pi-driver -m sg -h localhost -o 15 -p 31415" >> $HOME/opt/bin/i-pi
 chmod u+x $HOME/opt/bin/i-pi
