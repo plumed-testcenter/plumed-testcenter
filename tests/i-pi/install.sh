1fc35193cd256cbadc629c3297ce2f94928cdfe3
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-05-30 08:00:20 +0100

    Added some output in i-pi installation script

--- tests/i-pi/install.sh
+++ tests/i-pi/install.sh
@@ -9,6 +9,19 @@ cd ../../../../
 # Copy i-pi to $HOME/opt
 cp -pr i-pi $HOME/opt
 
+if [ ! -f $HOME/opt/i-pi/bin/i-pi ] ; then
+   echo could not find i-pi
+else 
+   echo installed i-pi in $HOME/opt/i-pi/bin/i-pi
+fi
+
+if [ ! -f $HOME/opt/i-pi/bin/i-pi-driver ] ; then
+   echo could not find i-pi-driver
+else 
+   echo installed i-pi-driver in $HOME/opt/i-pi/bin/i-pi-driver
+fi
+
+
 # Make a script to run i-pi
 echo "#!/bin/bash" > $HOME/opt/bin/i-pi
 echo "PYTHONPATH=$HOME/opt/lib/plumed$suffix/python" >> $HOME/opt/bin/i-pi
