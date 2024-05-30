6d6017d5722dc75eb4b146d8b55fcc3ec19e3c09
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-05-30 07:30:26 +0100

    Adjusted scripts for building i-pi

--- tests/i-pi/install.sh
+++ tests/i-pi/install.sh
@@ -4,14 +4,13 @@ git clone https://github.com/i-pi/i-pi.git
 # Build the fortran drivers
 cd i-pi/drivers/f90
 make
-cd ../../../
+cd ../../../../
 
-# Copy i-pi and i-pi driver to $HOME/opt/bin
-cp bin/i-pi $HOME/opt/bin/i-pi-code
-cp bin/i-pi-driver $HOME/opt/bin/
+# Copy i-pi to $HOME/opt
+cp -pr i-pi $HOME/opt
 
 # Make a script to run i-pi
 echo "#!/bin/bash" > $HOME/opt/bin/i-pi
 echo "PYTHONPATH=$HOME/opt/lib/plumed$suffix/python" >> $HOME/opt/bin/i-pi
-echo "$HOME/opt/bin/i-pi-code install.xml & sleep 5; $HOME/opt/bin/i-pi-driver -m sg -h localhost -o 15 -p 31415" >> $HOME/opt/bin/i-pi
+echo "$HOME/opt/i-pi/bin/i-pi install.xml & sleep 5; $HOME/opt/i-pi/bin/i-pi-driver -m sg -h localhost -o 15 -p 31415" >> $HOME/opt/bin/i-pi
 chmod u+x $HOME/opt/bin/i-pi
