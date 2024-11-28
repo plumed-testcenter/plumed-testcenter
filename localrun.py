1ecad4cd10e787f3f543f3cad5f738c69ffd4a76
Daniele <Iximiel@users.noreply.github.com>
2024-11-28 08:45:44 +0100

    Setting up an easier local-run with click (#12)
    Co-authored-by: Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>

--- localrun.py
+++ localrun.py
@@ -1,26 +1,52 @@
-import sys
-import getopt
+# used ruff 0.6.4 to check and format this
 import shutil
 import importlib
 from runtests import buildTestPages, runTests
+import click
 
-if __name__ == "__main__":
+
+@click.command()
+@click.argument("codedir", type=click.Path(exists=True))
+@click.option(
+    "--prefix", default="local_", help="The local run prefix for the data directories."
+)
+@click.option(
+    "--plumed",
+    "-p",
+    default=["plumed"],
+    help="The plumed executable(s) to run.",
+    multiple=True,
+)
+@click.option("--printJson", "printJson", is_flag=True, default=False)
+def localRun(codedir: str, prefix: str, plumed: "list[str]", printJson: bool):
+    """Simple local run CLI
+
+    Specify the directory in wich the settings for the test are stored as a first argument
+    For example: localrun.py tests/quantum_espresso
+
+    The plumed executable defaults to "plumed"
+
+    The default prefix is "local_" (files are stored in "local_pages", "local_tests",...)
+
+    Before running this you may need to manually remove some directories in "*prefix*tests".
+    An error message will be printed if the directory is not empty.
+    """
+    code: str = codedir.split("/")[-1]
     # to run PATH must contain the dir to plumed and the one to the executable of your code
     # code = "quantum_espresso"
-    code = "gmx24"
-    prefix = "local_"
-    plumedToRun = [{"plumed": "plumed", "printJson": False}]
+    plumedToRun = [{"plumed": p, "printJson": printJson} for p in plumed]
+
     print("Code: " + code)
     print("Preparing pages")
     # Engforce and engvir share the same procedure
     shutil.copy("pages/engforce.md", "pages/engvir.md")
-    buildTestPages("tests/" + code, prefix, plumedToRun)
+    buildTestPages(codedir, prefix, plumedToRun)
     # this usues > 50% of the time
     buildTestPages("pages", prefix, plumedToRun)
     # Create an __init__.py module for the desired code
-    with open("tests/" + code + "/__init__.py", "w") as ipf:
+    with open(codedir + "/__init__.py", "w") as ipf:
         ipf.write("from .mdcode import mdcode\n")
-    myMDcode = importlib.import_module("tests." + code, "mdcode")
+    myMDcode = importlib.import_module(codedir.replace("/", "."), "mdcode")
     # And create the class that interfaces with the MD code output
     runner = myMDcode.mdcode()
     # Now run the tests
@@ -33,3 +59,17 @@ if __name__ == "__main__":
         prefix=prefix,
         settingsFor_runMDCalc=dict(execNameChanged=False, makeArchive=False),
     )
+
+
+if __name__ == "__main__":
+    try:
+        localRun()
+    except FileExistsError as e:
+        print(e)
+        print(
+            "Please remove (and backup, if you need it) this directory before running again"
+        )
+        exit(1)
+    except Exception as e:
+        print(e)
+        exit(1)
