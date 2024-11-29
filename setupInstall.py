e897763cf27034f1fe754866337869e9893ec22d
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-11-29 17:17:05 +0100

    ready to merge

--- setupInstall.py
+++ setupInstall.py
@@ -9,7 +9,7 @@ def zip(path):
     with zipfile.ZipFile(path + ".zip", "w") as f_out:
         f_out.write(path)
     # I need the file in the ouput
-    # os.remove(path)
+    os.remove(path)
 
 def buildInstallPage( code ) :
    # Zip all the logs
@@ -22,16 +22,18 @@ def buildInstallPage( code ) :
    of.write("------------------------\n \n")
    stable_version=subprocess.check_output('plumed info --version', shell=True).decode('utf-8').strip()
    # Save the stable version of plumed to a file to pass to the update.py script (not classy Gareth)
-   vf = open("stable_version.md", "w+")
-   vf.write( stable_version )
-   vf.close() 
+   with open("stable_version.md", "w+") as vf:
+    vf.write( stable_version )
+   
    of.write("To compile " + code + " and PLUMED the following bash script was used.  " + code + " was statically linked with the v" + stable_version + " of PLUMED. In a separate build, the master version of PLUMED was linked to " + code + " as a runtime library. \n \n")
    of.write("Build with stable version download: [zipped raw stdout](stdout.txt.zip)  - [zipped raw stderr](stderr.txt.zip) \n \n")
    of.write("Build with master version download: [zipped raw stdout](stdout_master.txt.zip)  - [zipped raw stderr](stderr_master.txt.zip) \n\n")
    of.write("```bash\n")
-   sf = open("tests/" + code + "/install.sh" ,"r")
-   inp = sf.read()
-   for line in inp.splitlines() : of.write( line + "\n")
+
+   with open("tests/" + code + "/install.sh", "r") as sf:   
+    inp = sf.read()
+   for line in inp.splitlines() :
+      of.write( line + "\n")
    of.write("```\n\n")
    of.close()
 
