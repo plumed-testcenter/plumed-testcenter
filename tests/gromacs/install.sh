485dd79d9bf00d5c62facaca49386773f57ade42
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-11-29 12:57:18 +0100

    changed the script to be run by the stable test

--- tests/gromacs/install.sh
+++ tests/gromacs/install.sh
@@ -1,29 +1,41 @@
 # shellcheck disable=SC2154,2148
 # Cloning the gromacs repository
 # formatted with shfmt_v3.6.0
-git clone --depth 1 --branch v2024.2 https://gitlab.com/gromacs/gromacs.git
+# Clone direclty into the wanted gromacs version
+git clone --depth 1 --branch v2024.2 https://gitlab.com/gromacs/gromacs.git "gromacs$suffix"
 
-# Checkout the correct version of gromacs
-cd gromacs || exit 1
+# setting up the versionsuffix as in runtests scripts
+versionSuffix
+
+exeSuffix=v$("plumed$suffix" info --version)
+
+if suffix="_master"; then
+   exeSuffix=""
+fi
+
+cd "gromacs$suffix" || exit 1
 
 # Patch with PLUMED
 "plumed$suffix" patch --engine gromacs-2024.2 -p --mode "$mode"
 
+prefix=$HOME/opt/gromacs$suffix
+
 # Run cmake
 mkdir build
 cd build || exit 1
-cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_INSTALL_PREFIX="$HOME/opt/gromacs" -DGMX_ENABLE_CCACHE=ON
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_INSTALL_PREFIX="$prefix" -DGMX_ENABLE_CCACHE=ON
 # -j is atomatic number of concurrent cores
 cmake --build . -j4
 cmake --install .
 
-if [ -x "$HOME/opt/gromacs/bin/gmx" ]; then
+if [ -x "${prefix}/bin/gmx" ]; then
    # Write a script to execute gromacs calculations
-   cat <<EOF >"$HOME/opt/bin/gromacs"
+   cat <<EOF >"$HOME/opt/bin/gromacs$exeSuffix"
 #!/bin/bash
-"\$HOME/opt/gromacs/bin/gmx" grompp -p topol.top -c conf.gro -f md.mdp
-"\$HOME/opt/gromacs/bin/gmx" mdrun -nt 1 -plumed plumed.dat
-echo 5 | "\$HOME/opt/gromacs/bin/gmx" energy
+mygmx=\$HOME/opt/gromacs$suffix/bin/gmx
+"\$mygmx" grompp -p topol.top -c conf.gro -f md.mdp
+"\$mygmx" mdrun -nt 1 -plumed plumed.dat
+echo 5 | "\$mygmx" energy
 EOF
-   chmod u+x "$HOME/opt/bin/gromacs"
+   chmod u+x "$HOME/opt/bin/gromacs$exeSuffix"
 fi
