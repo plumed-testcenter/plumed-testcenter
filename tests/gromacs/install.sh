356deec2312d4100a9762fd6db2d403e0890acae
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-11-29 10:52:06 +0100

    now gromacs can run in parallel and exploit ccache

--- tests/gromacs/install.sh
+++ tests/gromacs/install.sh
@@ -1,27 +1,29 @@
+# shellcheck disable=SC2154
 # Cloning the gromacs repository
-git clone https://gitlab.com/gromacs/gromacs.git
+# formatted with shfmt_v3.6.0
+git clone --depth 1 --branch v2024.2 https://gitlab.com/gromacs/gromacs.git
 
 # Checkout the correct version of gromacs
-cd gromacs
-git checkout v2024.2
+cd gromacs || exit 1
 
 # Patch with PLUMED
-plumed$suffix patch --engine gromacs-2024.2 -p --mode $mode
+"plumed$suffix" patch --engine gromacs-2024.2 -p --mode "$mode"
 
 # Run cmake
 mkdir build
-cd build
-cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_INSTALL_PREFIX=$HOME/opt/gromacs 
+cd build || exit 1
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_INSTALL_PREFIX="$HOME/opt/gromacs" -DGMX_ENABLE_CCACHE=ON
+# -j is atomatic number of concurrent cores
+cmake --build . -j
+cmake --install .
 
-# Now make
-make
-make install
-
-if [ -f $HOME/opt/gromacs/bin/gmx ] ; then
+if [ -x "$HOME/opt/gromacs/bin/gmx" ]; then
    # Write a script to execute gromacs calculations
-   echo "#!/bin/bash" > $HOME/opt/bin/gromacs
-   echo $HOME/opt/gromacs/bin/gmx grompp -p topol.top -c conf.gro -f md.mdp >> $HOME/opt/bin/gromacs
-   echo $HOME/opt/gromacs/bin/gmx mdrun -nt 1 -plumed plumed.dat >> $HOME/opt/bin/gromacs
-   echo "echo 5 | $HOME/opt/gromacs/bin/gmx energy" >> $HOME/opt/bin/gromacs
-   chmod u+x $HOME/opt/bin/gromacs
+   cat <<EOF >"$HOME/opt/bin/gromacs"
+#!/bin/bash
+"\$HOME/opt/gromacs/bin/gmx" grompp -p topol.top -c conf.gro -f md.mdp
+"\$HOME/opt/gromacs/bin/gmx" mdrun -nt 1 -plumed plumed.dat
+echo 5 | "\$HOME/opt/gromacs/bin/gmx" energy
+EOF
+   chmod u+x "$HOME/opt/bin/gromacs"
 fi
