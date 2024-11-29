f578a817abb132441721dc175e214fc4c8e15011
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-11-29 11:54:05 +0100

    why exit code 143?

--- tests/gromacs/install.sh
+++ tests/gromacs/install.sh
@@ -1,4 +1,4 @@
-# shellcheck disable=SC2154
+# shellcheck disable=SC2154,2148
 # Cloning the gromacs repository
 # formatted with shfmt_v3.6.0
 git clone --depth 1 --branch v2024.2 https://gitlab.com/gromacs/gromacs.git
@@ -14,7 +14,7 @@ mkdir build
 cd build || exit 1
 cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_INSTALL_PREFIX="$HOME/opt/gromacs" -DGMX_ENABLE_CCACHE=ON
 # -j is atomatic number of concurrent cores
-cmake --build . -j
+cmake --build . -j4
 cmake --install .
 
 if [ -x "$HOME/opt/gromacs/bin/gmx" ]; then
