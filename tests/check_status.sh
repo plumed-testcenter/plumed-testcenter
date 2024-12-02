e8f68b6b495dd8e2ae7373950bc604dad9f14f91
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-12-02 10:31:08 +0100

    reordering branches

--- tests/check_status.sh
+++ tests/check_status.sh
@@ -18,14 +18,15 @@ done
 
 executible=$(grep executible "tests/$code/info.yml" | sed -e 's/executible: //')
 
+echo "Looking for '$HOME/opt/bin/$executible' or  for '$HOME/opt/bin/$executible$suffix'"
 #thest that at least the non suffixed version exists
-if [[ ! -x $HOME/opt/bin/$executible || ! -x $HOME/opt/bin/$executible$suffix ]]; then
-     echo "install_plumed$suffix: broken" >>"tests/$code/info.yml"
-else
+if [[ -x $HOME/opt/bin/$executible || -x $HOME/opt/bin/$executible$suffix ]]; then
      echo "install_plumed$suffix: working" >>"tests/$code/info.yml"
      # Ensures we do not overwrite master version of PLUMED when testing simplemd
      if [[ ! -x $HOME/opt/bin/$executible$suffix ]]; then
           #this should be set up in the install script
           cp "$HOME/opt/bin/$executible" "$HOME/opt/bin/$executible$suffix"
      fi
+else
+     echo "install_plumed$suffix: broken" >>"tests/$code/info.yml"
 fi
