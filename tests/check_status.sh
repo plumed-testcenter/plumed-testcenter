3bda504c65215702174eaa47d2c5f9e7598e9cda
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-12-02 09:39:15 +0100

    formatted and shellchecked  check_status

--- tests/check_status.sh
+++ tests/check_status.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+# formatted with shfmt_v3.6.0
 
-suffix=`plumed info --version`
-suffix=`echo _v$suffix`
+suffix=$(plumed info --version)
+suffix=_v$suffix
 set -x
 
-for opt
-do
-case "$opt" in
-  (suffix=*) suffix="${opt#suffix=}" ;;
-  (code=*)  code="${opt#code=}" ;;
-  (*) echo "unknown option $opt" ; exit 1 ;;
-esac
+for opt; do
+     case "$opt" in
+     suffix=*) suffix="${opt#suffix=}" ;;
+     code=*) code="${opt#code=}" ;;
+     *)
+          echo "unknown option $opt"
+          exit 1
+          ;;
+     esac
 done
 
-executible=`grep executible tests/$code/info.yml | sed -e s/"executible: "//`
+executible=$(grep "executible tests/$code/info.yml" | sed -e s/"executible: "//)
 
-if [ ! -f $HOME/opt/bin/$executible ] ; then
-     echo install_plumed$suffix: broken >> tests/$code/info.yml
+if [[ ! -f $HOME/opt/bin/$executible ]]; then
+     echo "install_plumed$suffix: broken" >>"tests/$code/info.yml"
 else
-     echo install_plumed$suffix: working >> tests/$code/info.yml
+     echo "install_plumed$suffix: working" >>"tests/$code/info.yml"
      # Ensures we do not overwrite master version of PLUMED when testing simplemd
-     if [ ! -f $HOME/opt/bin/$executible$suffix ] ; then
-          cp $HOME/opt/bin/$executible $HOME/opt/bin/$executible$suffix
+     if [[ ! -f $HOME/opt/bin/$executible$suffix ]]; then
+          cp "$HOME/opt/bin/$executible" "$HOME/opt/bin/$executible$suffix"
      fi
 fi
