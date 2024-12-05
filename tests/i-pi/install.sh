e1c4ede2711abe3de54434164a80675548061e32
Daniele Rapetti <iximiel@gmail.com>
2024-12-05 12:08:31 +0100

    now i-pi should run

--- tests/i-pi/install.sh
+++ tests/i-pi/install.sh
@@ -17,12 +17,20 @@ else
    echo DID NOT FIND PYTHON DIRECTORY FOR RUNNING PLUMED
 fi
 
+#temporary workaround
+#the interface should load the correct kernel, whatever its version
+# (but thil may look more like a 'user installation')
+pip install 'plumed'
+# TODO: decide to install the python integration direcly from the plumed repo
+
 executable=$HOME/opt/bin/i-pi
 # Make a script to run i-pi
 cat <<EOF >"$executable"
 #!/bin/bash
-export PYTHONPATH=$HOME/opt/lib/plumed$suffix/python
-$HOME/opt/i-pi/bin/i-pi input.xml & sleep 5
-$HOME/opt/i-pi/bin/i-pi-driver -m sg -h localhost -o 15 -p 31415
+#export PYTHONPATH=$HOME/opt/lib/plumed$suffix/python
+export PLUMED_KERNEL=$plumedKernel
+ipibin=$HOME/opt/i-pi/bin
+\${ipibin}/i-pi input.xml & sleep 5
+\${ipibin}/i-pi-driver -m sg -h localhost -o 15 -p 31415
 EOF
 chmod u+x "$executable"
