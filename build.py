cbdc7473a392acbba9e22a5c3a205d87fdd42e44
Daniele Rapetti <iximiel@gmail.com>
2024-12-05 09:56:51 +0100

    Now build truly checks for test directories

--- build.py
+++ build.py
@@ -4,26 +4,56 @@ import os
 from datetime import date
 
 
+def isTest(path) -> bool:
+    """
+    Check if a directory can be considered a test directory.
+    It is a test directory if it contains the three files:
+    - info.yml
+    - install.sh
+    - mdcode.py
+    """
+    if os.path.isdir(path):
+        for file in ["info.yml", "install.sh", "mdcode.py"]:
+            if not os.path.isfile(path + "/" + file):
+                return False
+        return True
+    return False
+
+
 def checkWorkflow():
     print("Checking Workflow")
-    f = open(".github/workflows/main.yml", "r")
-    inp = f.read()
-    f.close()
+    # this works with the replica list written like this
+    ###
+    # replica:
+    #  - "simplemd"
+    #  - "lammps"
+    #  - "quantum_espresso"
+    #  - "i-pi"
+    #  - "gromacs"
+    #  - "gromacs-vanilla"
+    ### or like this:
+    # replica: ["simplemd", "lammps", "quantum_espresso", "i-pi", "gromacs", "gromacs-vanilla"]
+    with open(".github/workflows/main.yml", "r") as f:
+        workflow = yaml.load(f, Loader=yaml.BaseLoader)
+        replicalist = workflow["jobs"]["build"]["strategy"]["matrix"]["replica"]
 
-    for line in inp.splitlines():
-        if "replica:" in line:
-            adirs = (
-                line.replace("replica:", "")
-                .replace("[", "")
-                .replace("]", "")
-                .replace(",", "")
-                .split()
-            )
-            rdirs = os.listdir("tests")
-            if rdirs != adirs:
-                ValueError(
-                    "Tests have not been updated. Run the command create_workflow.py in create_workflow directory and commit the changes to .github/actions/main.yml"
-                )
+        testdirs = [d for d in os.listdir("tests") if isTest("tests/" + d)]
+
+        replicalist = sorted(replicalist)
+        testdirs = sorted(testdirs)
+        if testdirs != replicalist:
+            error = "Tests have not been updated.\n"
+            if len(testdirs) > len(replicalist):
+                error += "The following tests are missing from the workflow file:\n"
+                for t in testdirs:
+                    if t not in replicalist:
+                        error += " - " + t + "\n"
+            else:
+                error += "The following test in the workflow do not have their own directory:\n"
+                for t in replicalist:
+                    if t not in testdirs:
+                        error += " - " + t + "\n"
+            raise ValueError(error)
 
 
 def buildBrowsePage(stable_version, tested):
@@ -37,12 +67,12 @@ def buildBrowsePage(stable_version, tested):
    |:-----------------|:------------------|:--------:|:------------:|"""
 
     for code in os.listdir("tests"):
-        if os.path.isfile("tests/" + code):
+        if  not isTest("tests/" + code):
             continue
         compile_badge, test_badge = "", ""
 
-        with open("tmp/extract/tests/" + code + "/info.yml", "r") as stram:
-            info = yaml.load(stram, Loader=yaml.BaseLoader)
+        with open("tmp/extract/tests/" + code + "/info.yml", "r") as stream:
+            info = yaml.load(stream, Loader=yaml.BaseLoader)
 
         for i in range(len(tested)):
             compile_badge += (
@@ -64,6 +94,7 @@ def buildBrowsePage(stable_version, tested):
                 f" [![tested on {tested[i]}](https://img.shields.io/badge/{tested[i]}-"
             )
             test_status = info["test_plumed" + tested[i]]
+
             if test_status == "working":
                 test_badge += "passing-green.svg"
             elif test_status == "partial":
@@ -72,9 +103,9 @@ def buildBrowsePage(stable_version, tested):
                 test_badge += "broken-red.svg"
             elif test_status == "failing":
                 test_badge += "failed-red.svg"
+
             if tested[i] != stable_version:
                 test_badge += f")](tests/{code}/testout_{tested[i]}.html)"
-
             else:
                 test_badge = test_badge + ")](tests/" + code + "/testout.html)"
         browse += f"| [{code}]({info['link']}) | {info['description']} | {compile_badge} | {test_badge} | \n"
@@ -105,10 +136,14 @@ When the tests above are run PLUMED is built using the install plumed action.
 
 
 if __name__ == "__main__":
-    # Check that the workflow matches with the directories
-    checkWorkflow()
-    # Build the page with all the MD codes
-    vf = open("tmp/extract/stable_version.md", "r")
-    stable_version = vf.read()
-    vf.close()
-    buildBrowsePage("v" + stable_version, ("v" + stable_version, "master"))
+    try:
+        # Check that the workflow matches with the directories
+        checkWorkflow()
+        # Build the page with all the MD codes
+        with open("tmp/extract/stable_version.md", "r") as vf:
+            stable_version = vf.read()
+
+        buildBrowsePage("v" + stable_version, ("v" + stable_version, "master"))
+    except Exception as e:
+        print(e)
+        exit(1)
