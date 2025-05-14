285df4611198a61156ef7e5ce666696d80f65a9d
Gareth Aneurin Tribello <garethtribello@gareths-mbp.mp.qub.ac.uk>
2025-05-14 09:30:16 +0100

    Added first go at runners for dlpoly

--- tests/dlpoly/install.sh
+++ tests/dlpoly/install.sh
@@ -1,25 +1,18 @@
 # Clone the dlpoly repository 
 repo=https://gitlab.com/ccp5/dl-poly.git
 sourcedir=dl-poly$suffix
-echo cloning dlpoly repository
 git clone --depth 1 "$repo" "$sourcedir"
-echo cloned dlpoly repository
 
 # Add plumed to PKG_CONFIG_PATH
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/opt/lib/pkgconfig
 
 # Change to dl-poly directory
-echo building dlpoly
 cd dl-poly$suffix
 mkdir build-mpi
 cd build-mpi
-echo created dlpoly directory
 FFLAGS="-O3 -fallow-argument-mismatch" cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_PLUMED=ON -DINTERNAL_PLUMED=OFF &> cmake.log
-echo ran cmake
 cat cmake.log
-echo Making code
 make &> make.log
-echo built dlpoly
 cat make.log
 
 if [[ -x bin/DLPOLY.Z ]]; then
--- tests/dlpoly/mdcode.py
+++ tests/dlpoly/mdcode.py
@@ -5,26 +5,75 @@ import subprocess
 class mdcode :
    def setParams( self ) :
        params = {
-         "temperature": 1.0,
-         "tstep": 0.005,
-         "relaxtime": 1.0,
-         "pressure": 1.0,
-         "prelaxtime": 4
+         "temperature": 300,
+         "tstep": 0.001,
+         "relaxtime": 0.5,
+         "pressure": 0.0,
+         "prelaxtime": 1 
        }
        return params
  
    def runMD( self, mdparams ) :
-       return 1
+       ensemble_stuff = """
+ensemble                      nvt
+ensemble_method               hoover
+ensemble_thermostat_coupling  {mdparams["relaxtime"} ps
+"""
+       if mdparams["ensemble"]=="npt" :
+          ensemble_stuff = """
+ensemble                      npt
+ensemble_method               hoover
+ensemble_thermostat_coupling  {mdparams["relaxtime"} ps
+ensemble_barostat_coupling    {mdparams["pelaxtime"} ps
+"""
+       inp = """
+title                 PLUMED test calc
+io_file_config        CONFIG
+io_file_field         FIELD
+io_file_statis        STATIS
+io_file_revive        REVIVE
+io_file_revcon        REVCON
+io_file_tabvdw        TABLE
+temperature           {mdparams["temperature"]} K
+plumed                on
+plumed_input          plumed.dat
+print_frequency       1 steps
+stats_frequency       1 steps
+vdw_cutoff            8.0 ang
+padding               0.25 ang
+cutoff                8.0 ang
+coul_method           spme
+spme_precision        1e-06
+time_run              {mdparams["nsteps"]} steps
+time_equilibration    0 steps
+time_job              3600.0 s
+time_close            100.0 s
+timestep              {mdparams["tstep"]} ps
+pressure_hydrostatic  0.0 katm
+rescale_frequency     10 steps
+{ensemble_stuff}
+"""
+       # Work out the name of the dlpoly executable
+       executible = mdparams["executible"]
+       # Now run the calculation using subprocess
+       with open("stdout","w") as stdout:
+        with open("stderr","w") as stderr:
+          out = subprocess.run([executible], text=True, input=inp, stdout=stdout, stderr=stderr )
+       return out.returncode
 
    def getTimestep( self ) :
-       return 0.005
+       return 0.001
 
    def getNumberOfAtoms( self, rundir ) :
-       natoms = 1 
+       natoms, traj = [], mda.coordinates.DLPoly.HistoryReader( rundir + "/HISTORY" )
+       for frame in traj.trajectory : natoms.append( frame.positions.shape[0] ) 
        return natoms 
 
    def getPositions( self, rundir ) :
-       pos = np.zeros([1,3])
+       first, traj = True, mda.coordinates.DLPoly.HistoryReader( rundir + "/HISTORY" )
+       for frame in traj.trajectory :
+          if first : pos, first = 0.1*frame.positions.copy(), False
+          else : pos = np.concatenate( (pos, 0.1*frame.positions), axis=0 ) 
        return pos
 
    def getCell( self, rundir ) :
@@ -32,10 +81,22 @@ class mdcode :
        return cell
 
    def getMasses( self, rundir ) :
-       return np.ones( natoms[0] )
+       masses = np.zeros(4*64)
+       for i in range(64) : 
+           masses[4*i+0] = 107.868
+           masses[4*i+1] = 126.905
+           masses[4*i+2] = 107.868
+           masses[4*i+3] = 126.905  
+       return masses
 
    def getCharges( self, rundir ) :
-       return np.zeros( natoms[0] )
+       charges = np.zeros(4*64)
+       for i in range(64) : 
+           charges[4*i+0] = 0.6 
+           charges[4*i+1] = -0.6 
+           charges[4*i+2] = 0.6 
+           charges[4*i+3] = -0.6  
+       return charges
 
    def getEnergy( self, rundir ) :
        return 1
