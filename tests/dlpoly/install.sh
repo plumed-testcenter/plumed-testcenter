dde39c72a213ba8d99570d51e7f2a67d6c5ffe27
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2025-05-15 17:03:16 +0200

    workaround to test master with dlpoly

--- tests/dlpoly/install.sh
+++ tests/dlpoly/install.sh
@@ -1,4 +1,8 @@
-# Clone the dlpoly repository 
+#This script will be embedded in one that has the relevant variables set:
+#shellcheck disable=2154,2148
+#formatted with shfmt_v3.6.0
+
+# Clone the dlpoly repository
 repo=https://gitlab.com/ccp5/dl-poly.git
 sourcedir=dl-poly$suffix
 git clone --depth 1 "$repo" "$sourcedir"
@@ -7,12 +11,27 @@ git clone --depth 1 "$repo" "$sourcedir"
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/opt/lib/pkgconfig
 
 # Change to dl-poly directory
-cd dl-poly$suffix
+cd "dl-poly$suffix" || {
+   echo "Something failed with cloning dl-poly into dl-poly$suffix" >&2
+   exit 1
+}
+
+if [[ -n ${suffix} ]]; then
+   #this "patches" the pkg-config search in cmake
+   #here I am assuming that ${suffix} is appended also to plumed.pc (like plumed_master.pc)
+   sed -i CMakeLists.txt \
+      -e "s/pkg_check_modules(PLUMED REQUIRED plumed>=\${PLUMED_VERSION})/pkg_check_modules(PLUMED REQUIRED plumed${suffix}>=\${PLUMED_VERSION})/"
+fi
+
 mkdir build-mpi
-cd build-mpi
-FFLAGS="-O3 -fallow-argument-mismatch" cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_PLUMED=ON -DINTERNAL_PLUMED=OFF &> cmake.log
+cd build-mpi || {
+   echo "cannot cd to build-mpi" >&2
+   exit 1
+}
+
+FFLAGS="-O3 -fallow-argument-mismatch" cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_PLUMED=ON -DINTERNAL_PLUMED=OFF &>cmake.log
 cat cmake.log
-make &> make.log
+make &>make.log
 cat make.log
 
 if [[ -x bin/DLPOLY.Z ]]; then
